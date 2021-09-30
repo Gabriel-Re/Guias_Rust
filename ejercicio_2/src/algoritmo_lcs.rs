55fc746f1324bd712682154edeaee1cee9d2ef5f
Gabriel-Re <gre@fi.uba.ar>
2021-09-29 21:50:36 -0300

    Implemento diff con errores

--- ejercicio_2/src/algoritmo_lcs.rs
+++ ejercicio_2/src/algoritmo_lcs.rs
@@ -10,11 +10,57 @@ notar que la subsecuencia no necesita ser consecutiva, sino que debe estar en or
 // https://www.youtube.com/watch?v=NnD96abizww -> video con la posta
 pub mod longestcommonsubsequence{
 
-    pub fn lcs(){
+    pub fn construir_diff(matriz_secuencias: Vec<Vec<i32>>, lineas_archivo1: &Vec<char>,
+        lineas_archivo2: &Vec<char>, longitud_archivo1:usize, longitud_archivo2:usize){
 
+        if longitud_archivo1 > 0 && longitud_archivo2 > 0 && 
+        lineas_archivo1[longitud_archivo1 - 1] == lineas_archivo2[longitud_archivo2 - 1]{
+
+            construir_diff(matriz_secuencias, lineas_archivo1, lineas_archivo2
+                , longitud_archivo1-1, longitud_archivo2-1); //ok
+        
+            println!("> {}",lineas_archivo2[longitud_archivo1-1]);//ok
+
+        }else if longitud_archivo2 > 0 && (longitud_archivo1 == 0 || matriz_secuencias[longitud_archivo1][longitud_archivo2-1] 
+            >= matriz_secuencias[longitud_archivo1-1][longitud_archivo2]){
+
+            construir_diff(matriz_secuencias, lineas_archivo1, lineas_archivo2
+                , longitud_archivo1, longitud_archivo2-1); //ok
+            println!("> {}",lineas_archivo2[longitud_archivo2-1]);// ok
+
+        }else if longitud_archivo1 > 0 && (longitud_archivo2 == 0 || matriz_secuencias[longitud_archivo1][longitud_archivo2-1] 
+            < matriz_secuencias[longitud_archivo1-1][longitud_archivo2]){
+
+            construir_diff(matriz_secuencias, lineas_archivo1, lineas_archivo2
+                , longitud_archivo1-1, longitud_archivo2); // ok
+            println!("< {}",lineas_archivo1[longitud_archivo1-1]); //oj
+
+        }else{
+            println!("");
+        }
     }
 
-    pub fn creacion_matriz_secuencias(vector_lineas_archivo1: &Vec<String>, vector_lineas_archivo2: &Vec<String>) 
+    // C es la grilla computada por lcs()
+// X e Y son las secuencias
+// i y j especifican la ubicacion dentro de C que se quiere buscar cuando 
+//    se lee el diff. Al llamar a estar funcion inicialmente, pasarle
+//    i=len(X) y j=len(Y) // i es long1 y j es long2
+/*
+function print_diff(C, X, Y, i, j)
+if i > 0 and j > 0 and X[i-1] = Y[j-1]
+    print_diff(C, X, Y, i-1, j-1)
+    print "  " + X[i-1]
+else if j > 0 and (i = 0 or C[i,j-1] >= C[i-1,j])
+    print_diff(C, X, Y, i, j-1)
+    print "> " + Y[j-1]
+else if i > 0 and (j = 0 or C[i,j-1] < C[i-1,j])
+    print_diff(C, X, Y, i-1, j)
+    print "< " + X[i-1]
+else
+    print ""
+*/
+
+    pub fn creacion_matriz_secuencias(vector_lineas_archivo1: &Vec<char>, vector_lineas_archivo2: &Vec<char>) 
     -> Vec<Vec<i32>> {
         let largo_archivo1 = vector_lineas_archivo1.len();
         let largo_archivo2 = vector_lineas_archivo2.len();
@@ -37,7 +83,7 @@ pub mod longestcommonsubsequence{
                 if vector_lineas_archivo1[i] == vector_lineas_archivo2[j]{
                     matriz_secuencias[i+1][j+1] = matriz_secuencias[i][j] + 1;
                 }else{
-                    if(matriz_secuencias[i+1][j] > matriz_secuencias[i][j+1]){
+                    if matriz_secuencias[i+1][j] > matriz_secuencias[i][j+1]{
                         matriz_secuencias[i+1][j+1] = matriz_secuencias[i+1][j];
                     }else{
                         matriz_secuencias[i+1][j+1] = matriz_secuencias[i][j+1];
@@ -53,7 +99,7 @@ pub mod longestcommonsubsequence{
     let X and Y be sequences
     let m be the length of X, and let n be the length of Y
 
-    C = grid(m+1, n+1)
+    C = matriz_secuencias(m+1, n+1)
     // recordar que .., es inclusivo para el límite inferior, pero excluye al superior
     for i := 0..m+1
         C[i,0] = 0
--- ejercicio_2/src/main.rs
+++ ejercicio_2/src/main.rs
@@ -13,9 +13,36 @@ mod algoritmo_lcs;
 
 fn main() {
    
-    let vector1: Vec<String> = manejo_archivos::operar_archivos::read_files_lines(RUTA1);
-    let vector2: Vec<String> = manejo_archivos::operar_archivos::read_files_lines(RUTA2);
+    let lineas_archivo1: Vec<String> = manejo_archivos::operar_archivos::read_files_lines(RUTA1);
+    let lineas_archivo2: Vec<String> = manejo_archivos::operar_archivos::read_files_lines(RUTA2);
+    
+    let largo_archivo1 = lineas_archivo1.len();
+    let largo_archivo2 = lineas_archivo2.len();
 
-    let matriz_secuencias: Vec<Vec<i32>> = algoritmo_lcs::longestcommonsubsequence::creacion_matriz_secuencias(&vector1, &vector2);
+    let mut largo_iterable = 0;
+
+    if largo_archivo1 <= largo_archivo2{
+        largo_iterable = largo_archivo1;
+    }else{
+        largo_iterable = largo_archivo1;
+    }
+
+    for i in 0..largo_iterable{
+        let lista_archivo1:Vec<char> = lineas_archivo1[i].chars().collect();
+        let long1 = lista_archivo1.len();
+        let lista_archivo2:Vec<char> = lineas_archivo2[i].chars().collect();
+        let long2 = lista_archivo2.len();        
+        
+        let mut matriz_secuencias: Vec<Vec<i32>> = algoritmo_lcs::longestcommonsubsequence::creacion_matriz_secuencias(&lista_archivo1, &lista_archivo2);
+        
+
+        algoritmo_lcs::longestcommonsubsequence::construir_diff(matriz_secuencias, &lista_archivo1, 
+            &lista_archivo2, largo_archivo1, largo_archivo2);
+
+    }
+
+    
+
+
 
 }
