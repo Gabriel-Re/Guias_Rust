e54687465dd4d981ddb81b2fd1280f02f66908f7
Gabriel-Re <gre@fi.uba.ar>
2021-10-05 10:02:24 -0300

    Creo ejercicio4 Guia4

---
+++ ejercicio_1/target/rls/debug/deps/ejercicio_1-b62d0556cd1bddcf.d
@@ -0,0 +1,5 @@
+/home/gabore/Escritorio/Facu/Taller/Guias_Rust/Guias_4/ejercicio_1/target/rls/debug/deps/ejercicio_1-b62d0556cd1bddcf.rmeta: src/main.rs
+
+/home/gabore/Escritorio/Facu/Taller/Guias_Rust/Guias_4/ejercicio_1/target/rls/debug/deps/ejercicio_1-b62d0556cd1bddcf.d: src/main.rs
+
+src/main.rs:
---
+++ ejercicio_1/target/rls/debug/deps/ejercicio_1-f079761b3943ec24.d
@@ -0,0 +1,5 @@
+/home/gabore/Escritorio/Facu/Taller/Guias_Rust/Guias_4/ejercicio_1/target/rls/debug/deps/ejercicio_1-f079761b3943ec24.rmeta: src/main.rs
+
+/home/gabore/Escritorio/Facu/Taller/Guias_Rust/Guias_4/ejercicio_1/target/rls/debug/deps/ejercicio_1-f079761b3943ec24.d: src/main.rs
+
+src/main.rs:
